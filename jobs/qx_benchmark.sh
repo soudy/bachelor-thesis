af6fc8b10715d611ff79468958d3cfc17204c18e
Steven Oud <soud@protonmail.com>
2021-04-11 23:39:47 +0200

    More repetitions for QX benchmark

--- jobs/qx_benchmark.sh
+++ jobs/qx_benchmark.sh
@@ -14,13 +14,15 @@ DATA_DIR="$HOME/bachelor-thesis/data/qx_benchmark_cqasm"
 OUT_DIR="$HOME/qx_benchmark"
 
 cores=(16 14 12 10 8 6 4 2 1)
-ns=(4 6 8 10 12 14 16 18 20 22 24 26 28 30)
+ns=(4 6 8 10 12 14 16 18 20 22 24 26 28 30 31)
+reps=50
 
 for core in "${cores[@]}"; do
     for n in "${ns[@]}"; do
         python $SCRIPT_PATH \
 	       	"$OUT_DIR/qaoa_n${n}_cores${core}.npz" \
-		"$DATA_DIR/qaoa_n${n}_p1_d2.qc" \
-		$core
+          "$DATA_DIR/qaoa_n${n}_p1_d2.qc" \
+		      $core \
+          --reps=$reps
     done
 done
