1817e390408adb39be8f742922476664cd4aee7d
Steven Oud <soud@protonmail.com>
2021-04-14 17:06:56 +0200

    Normal partition benchmark

--- scripts/qx_benchmark.py
+++ scripts/qx_benchmark.py
@@ -8,10 +8,25 @@ import time
 
 import numpy as np
 
+
+fat_soil_affinities = [
+    0, 4, 8, 12, 16, 20, 24, 28, 32, 36,  # node 0
+    1, 5, 9, 13, 17, 21, 25, 29, 33, 37,  # node 1
+    2, 6, 10, 14, 18, 22, 26, 30, 34, 38, # node 2
+    3, 7, 11, 15, 19, 23, 27, 31, 35, 39  # node 3
+]
+
+
+def get_core_affinities(n):
+    return ",".join(map(str, fat_soil_affinities[:n]))
+
+
 def main(args):
+    affinities = get_core_affinities(args["omp_threads"])
+
     env = os.environ.copy()
     env["OMP_NUM_THREADS"] = str(args["omp_threads"])
-    env["KMP_AFFINITY"] = "granularity=fine,scatter"
+    env["KMP_AFFINITY"] = f"granularity=fine,explicit,proclist=[{affinities}]"
     # env["OMP_PLACES"] = "cores"
     # env["OMP_PROC_BIND"] = "spread"
 
@@ -24,6 +39,7 @@ def main(args):
     for _ in range(args["reps"]):
         call_args = ["qx-simulator", args["cqasm"], "1", str(args["omp_threads"])]
         print(f"Running {call_args}")
+        print(f"\twith env: {env}")
 
         usage_start = resource.getrusage(resource.RUSAGE_CHILDREN)
         start_time = time.time()
@@ -42,7 +58,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="QX simulator benchmark script",
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("output", type=str, help="output file to write results to")
     parser.add_argument("cqasm", type=str, help="path to the cQASM file to benchmark")
